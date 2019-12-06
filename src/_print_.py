1e7a3c5a9957c19f0fdce8e1bf69413a7cc553a8
naysok <ysok.ysok@gmail.com>
2019-12-07 01:22:14 +0900

    [add] python scripts

---
+++ src/_print_.py
@@ -0,0 +1,7 @@
+import datetime
+
+
+print("Hello Wolrd")
+
+dt_now = datetime.datetime.now()
+print(dt_now)
---
+++ src/_while_.py
@@ -0,0 +1,18 @@
+print("\n*** APP START ***\n")
+
+
+while True:
+    
+    _f = input("Continue : ")
+
+    if _f == "q" or _f == "Q":
+        break
+    
+    else:
+        print("")
+        msg = input("input msg : ")
+        print(">>", msg, "\n")
+
+
+
+print("\n*** APP END ***\n")
